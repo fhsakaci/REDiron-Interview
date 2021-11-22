58f144bd62c64849969035a2fb53549a7988dac5
fhsakaci <furkan.sakaci@ogr.sakarya.edu.tr>
2021-11-22 18:32:06 +0300

    Message has been get

--- main.py
+++ main.py
@@ -15,36 +15,25 @@ select_box = driver.find_element_by_id("navbarDropdown")
 select_box.click()
 time.sleep(1)
 driver.find_element_by_partial_link_text("Sign In").click()
-#time.sleep(20)
+time.sleep(10)
 
-#driver.switch_to.frame(driver.find_element_by_xpath("//iframe[@title='Sign in or Create an Account']"))
-#print(driver.page_source)
-#time.sleep(1)
+driver.switch_to.frame(driver.find_element_by_xpath("//iframe[@title='Sign in or Create an Account']"))
 
-WebDriverWait(driver, 20).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH, "//iframe[@title='Sign in or Create an Account']")))
-WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "loginForgotPassword"))).click()
-WebDriverWait(driver, 20).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH, "//iframe[@title='Password Assistance']")))
+time.sleep(10)
 
-#test = driver.find_element_by_id("loginForgotPassword")
-#test.click()
+element = driver.find_element_by_id("loginForgotPassword") 
+driver.execute_script("arguments[0].click();", element)
+time.sleep(5)
 
-#driver.switch_to_default_content()
+driver.switch_to_default_content()
+time.sleep(5)
+driver.switch_to.frame(driver.find_element_by_xpath("//iframe[@title='Password Assistance']"))
+inputElement = driver.find_element_by_id("email")
+inputElement.send_keys('furkan.sakaci@gmail.com')
+driver.find_element_by_id('forgotPasswordForm').submit()
 
-#print(driver.page_source)
-#time.sleep(10)
+time.sleep(4)
+error = driver.find_element_by_id('passwordAssistantErr')
+print(error.text)
 
-#driver.switch_to.frame(driver.find_element_by_xpath("//iframe[@title='Password Assistance']"))
-#print(driver.page_source)
-time.sleep(1)
-
-
-#WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "li.navigationbar__item a[data-link-name=' My Account'][href='/Sign In']"))).click()
-
-#link = driver.find_element_by_xpath("//nav/div/ul/li[2]/a").click()
-#elem = driver.find_element_by_name("q")
-#elem.clear()
-#elem.send_keys("pycon")
-#elem.send_keys(Keys.RETURN)
-#assert "No results found." not in driver.page_source
-#time.sleep(100)
 driver.close()
